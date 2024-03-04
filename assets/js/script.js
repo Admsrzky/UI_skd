ca9faaec659271fab72d8fa7a0f16a20681efeed
Admsrzky <adimasrizki926@gmail.com>
2024-03-04 14:54:33 +0700

    new fitur

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// dark mode toggle
+    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+    // Change the icons inside the button based on previous settings
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        themeToggleLightIcon.classList.remove('hidden');
+    } else {
+        themeToggleDarkIcon.classList.remove('hidden');
+    }
+
+    var themeToggleBtn = document.getElementById('theme-toggle');
+
+    themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
+
+
+// Dropdown
+const dropdown = new HSDropdown(document.querySelector('#hs-dropdown'));
+const openBtn = document.querySelector('#open-btn');
+
+openBtn.addEventListener('click', () => {
+  dropdown.open();
+});
+
+const openBtyn = document.querySelector('#open-btn');
+
+openBtyn.addEventListener('click', () => {
+  HSDropdown.show('#hs-dropdown');
+});
